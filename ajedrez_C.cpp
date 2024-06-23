80ea809df867e50af83e88ea737881fdbb30fc80
EnzoStigliano <131629043+EnzoStigliano@users.noreply.github.com>
2024-06-23 00:08:14 -0300

    Lo que hice hoy
    Las funciones/procedimientos que no tengan un comentario arriba diciendo lo que hace están incompletas

--- ajedrez_C.cpp
+++ ajedrez_C.cpp
@@ -1,13 +1,8 @@
 #include <iostream>
-
+#include <array>
 #include <string>
 
 using namespace std;
-
-void verificarMate(int color){
-}
-
-void moverPieza(int tablero[8][8], int id) {};
 
 //Limpiar la pantalla
 void clearScreen() {
@@ -34,13 +29,23 @@ class Pawn: public ChessPiece {
     int movs;
 };
 
+class Tower: public ChessPiece {
+    int movs;
+};
+
 class King: public ChessPiece {
     int movs;
 };
 
 class Board {
-    public: int functionalBoard[8][8];
-    int nonVisibleBoard[8][8];
+    public: 
+    int functionalBoard[8][8];
+    int piecesBoard[8][8];
+    int menaceWhiteBoard[8][8];
+    int menaceBlackBoard[8][8];
+    int i;
+    int j;
+    string pieza;
     Pawn p1();
     Pawn p2();
     Pawn p3();
@@ -57,14 +62,18 @@ class Board {
     Pawn p14();
     Pawn p15();
     Pawn p16();
+    Tower t1();
+    Tower t2();
+    Tower t3();
+    Tower t4();
     King kg1();
     King kg2();
 
-    //Poner patrón de 0 y 1 en functionalBoard y rellenar de 2 nonVisibleBoard
-    void generateChessBoard() {
+    //Poner patrón de 0 y 1 en functionalBoard y rellenar de 2 piecesBoard
+    void generateBoard() {
         for (int i = 0; i < 8; i++) {
             for (int j = 0; j < 8; j++) {
-                nonVisibleBoard[i][j] = 2;
+                piecesBoard[i][j] = 2;
 
                 if ((i + j) % 2 == 0) functionalBoard[i][j] = 1;
                 else functionalBoard[i][j] = 0;
@@ -72,90 +81,162 @@ class Board {
         }
     }
 
-    //Poner las id de las piezas en nonVisibleBoard
-    void colocatePiecesInBoard() {
-        //Peones blancos
-        int i;
-
+    //Poner las id de las piezas en piecesBoard
+    void colocatePieces() {
         //Peones blancos
         for (i = 1; i < 9; i++) {
-            nonVisibleBoard[6][i-1] = 210 + i;
+            piecesBoard[6][i-1] = 210 + i;
         }
 
         //Peones negros
         for (i = 1; i < 9; i++) {
-            nonVisibleBoard[1][i-1] = 200 + i;
+            piecesBoard[1][i-1] = 200 + i;
         }
 
         //Reyes
-        nonVisibleBoard[0][3] = 70;
-        nonVisibleBoard[7][4] = 71;
+        piecesBoard[0][3] = 70;
+        piecesBoard[7][4] = 71;
 
         //Reinas
-        nonVisibleBoard[0][4] = 60;
-        nonVisibleBoard[7][3] = 61;
+        piecesBoard[0][4] = 60;
+        piecesBoard[7][3] = 61;
 
         //Alfiles
-        nonVisibleBoard[0][2] = 501;
-        nonVisibleBoard[0][5] = 502;
-        nonVisibleBoard[7][2] = 511;
-        nonVisibleBoard[7][5] = 512;
+        piecesBoard[0][2] = 501;
+        piecesBoard[0][5] = 502;
+        piecesBoard[7][2] = 511;
+        piecesBoard[7][5] = 512;
 
         //Torres
-        nonVisibleBoard[0][0] = 401;
-        nonVisibleBoard[0][7] = 402;
-        nonVisibleBoard[7][0] = 411;
-        nonVisibleBoard[7][7] = 412;
+        piecesBoard[0][0] = 401;
+        piecesBoard[0][7] = 402;
+        piecesBoard[7][0] = 411;
+        piecesBoard[7][7] = 412;
 
         //Caballos
-        nonVisibleBoard[0][1] = 301;
-        nonVisibleBoard[0][6] = 302;
-        nonVisibleBoard[7][1] = 311;
-        nonVisibleBoard[7][6] = 312;
+        piecesBoard[0][1] = 301;
+        piecesBoard[0][6] = 302;
+        piecesBoard[7][1] = 311;
+        piecesBoard[7][6] = 312;
     }
 
     //Imprimir tablero
-    void printChessBoard() {
+    void printBoard() {
         cout << endl << " +---+---+---+---+---+---+---+---+" << endl;
-        for (int i = 0; i < 8; i++) {
-            for (int j = 0; j < 8; j++) {
-                if(nonVisibleBoard[i][j] != 2) {cout << " | " << nonVisibleBoard[i][j];}
+        for (i = 0; i < 8; i++) {
+            for (j = 0; j < 8; j++) {
+                if(piecesBoard[i][j] != 2) {cout << " | " << piecesBoard[i][j];}
                 else cout << " | " << functionalBoard[i][j];
             }
             cout << " |" << endl << " +---+---+---+---+---+---+---+---+" << endl;
         }
     }
+
+    void menacePositions(int color){
+
+    for(i = 0; i < 8; i++){
+        for(j = 0; j < 8; j++){
+            menaceWhiteBoard[i][j] = 0;
+            menaceBlackBoard[i][j] = 0;
+        }
+    }
+
+    for(i = 0; i < 8; i++){
+        for(j = 0; j < 8; j++){
+            pieza = to_string(piecesBoard[i][j]);
+            if(pieza[1] == color){
+                switch(pieza[0]){
+                    case 2:
+                        break;
+                    case 3:
+                        break;
+                    case 4:
+                        break;
+                    case 5:
+                        break;
+                    case 6:
+                        break;
+                    case 7:
+                        break;
+                }
+            }
+        }
+    }
+}
+
+    //Verificar si el rey de un color está en jaque
+    int verificarJaque(int color){
+    for(i = 0; i < 8; i++){
+        for(j = 0; j < 8; j++){
+            pieza = to_string(piecesBoard[i][j]);
+            if(pieza[0] == 7 && pieza[1] == color && (menaceWhiteBoard[i][j] == 1 || menaceBlackBoard[i][j] == 1)){
+                return 1; //Está en jaque el rey
+            }
+        }
+    }
+
+    return 0; //No está en jaque
+}
+
+    int verificarMate(int color){
+        int check = 0;
+
+        for(i = 0; i < 8; i++){
+            for(j = 0; j < 8; j++){
+                pieza = to_string(piecesBoard[i][j]);
+                if(pieza[0] == 7 && pieza[1] == !color){
+                    break;
+                }
+            }
+        }
+
+        menacePositions(color);
+
+        if(color){
+            if(i > 0){
+                //Fijarse arriba
+                if(j > 0){
+                    //Fijarse izquierda
+                    //Fijarse arriba a la izquierda
+                }
+            }
+        }
+}
+
+    void moverPieza(int tablero[8][8], int id) {
+    };
 };
 
 int main(void) {
+    int color;
     Board b;
-    b.generateChessBoard();
-    b.colocatePiecesInBoard();
-    b.printChessBoard();
+    b.generateBoard();
+    b.colocatePieces();
+    b.printBoard();
 
     //verificarMate(color){};
-    //verificarJaque();
+    b.verificarJaque(color);
     //calcularCasillas(id);
     //moverPieza();
     /*
-    int turn = 1;
+    int color = 1;
     bool game_over = false;
 
     //Turnos
     do {
         clearScreen();
-        b.printChessBoard();
+        b.printBoard();
         int startX, startY, endX, endY;
         cout << "Turno del jugador " << turn << " (Formato: posicionx1 posiciony1 posicionx2 posiciony2): ";
         cin >> startX >> startY >> endX >> endY;
 
-        if (b.movePiece(startX, startY, endX, endY, turn)) {
+        if (b.movePiece(startX, startY, endX, endY, color)) {
             clearScreen();
-            b.printChessBoard();
-            cout << "El rey ha sido capturado. Jugador " << turn << " gana!" << endl;
+            b.printBoard();
+            cout << "El rey ha sido capturado. Jugador " << color << " gana!" << endl;
             game_over = true;
         } else {
-            turn = (turn == 1) ? 2 : 1;
+            color = (color == 1) ? 2 : 1;
         }
     } while (!game_over);
     */
