ffac0f8ca0005370da92497e112bb2351501772f
EnzoStigliano <131629043+EnzoStigliano@users.noreply.github.com>
2024-06-26 15:06:11 -0300

    Saco las clases de ChessPieces

--- src/Board.h
+++ src/Board.h
@@ -12,28 +12,6 @@ class Board {
     int i;
     int j;
     string pieza;
-    Pawn p1();
-    Pawn p2();
-    Pawn p3();
-    Pawn p4();
-    Pawn p5();
-    Pawn p6();
-    Pawn p7();
-    Pawn p8();
-    Pawn p9();
-    Pawn p10();
-    Pawn p11();
-    Pawn p12();
-    Pawn p13();
-    Pawn p14();
-    Pawn p15();
-    Pawn p16();
-    Tower t1();
-    Tower t2();
-    Tower t3();
-    Tower t4();
-    King kg1();
-    King kg2();
 
     //Poner patrón de 0 y 1 en functionalBoard y rellenar de 2 piecesBoard
     void generateBoard();
