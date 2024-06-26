a9cef7627ae583511426c6793c32f7a6cc5d5f98
EnzoStigliano <131629043+EnzoStigliano@users.noreply.github.com>
2024-06-26 14:40:53 -0300

    Saqué las clases de ChessPieces

--- src/Board.cpp
+++ src/Board.cpp
@@ -1,43 +1,17 @@
 class Board {
     public: 
-    int functionalBoard[8][8];
     int piecesBoard[8][8];
     int menaceWhiteBoard[8][8];
     int menaceBlackBoard[8][8];
     int i;
     int j;
     string pieza;
-    Pawn p1();
-    Pawn p2();
-    Pawn p3();
-    Pawn p4();
-    Pawn p5();
-    Pawn p6();
-    Pawn p7();
-    Pawn p8();
-    Pawn p9();
-    Pawn p10();
-    Pawn p11();
-    Pawn p12();
-    Pawn p13();
-    Pawn p14();
-    Pawn p15();
-    Pawn p16();
-    Tower t1();
-    Tower t2();
-    Tower t3();
-    Tower t4();
-    King kg1();
-    King kg2();
 
-    //Poner patrón de 0 y 1 en functionalBoard y rellenar de 2 piecesBoard
+    //Rellenar de 2 piecesBoard
     void generateBoard() {
         for (int i = 0; i < 8; i++) {
             for (int j = 0; j < 8; j++) {
                 piecesBoard[i][j] = 2;
-
-                if ((i + j) % 2 == 0) functionalBoard[i][j] = 1;
-                else functionalBoard[i][j] = 0;
             }
         }
     }
@@ -46,17 +20,17 @@ class Board {
     void colocatePieces() {
         //Peones blancos
         for (i = 1; i < 9; i++) {
-            piecesBoard[6][i-1] = 210 + i;
+            piecesBoard[6][i-1] = 2100 + 10 * i;
         }
 
         //Peones negros
         for (i = 1; i < 9; i++) {
-            piecesBoard[1][i-1] = 200 + i;
+            piecesBoard[1][i-1] = 2000 + 10 * i;
         }
 
         //Reyes
-        piecesBoard[0][3] = 70;
-        piecesBoard[7][4] = 71;
+        piecesBoard[0][3] = 700;
+        piecesBoard[7][4] = 710;
 
         //Reinas
         piecesBoard[0][4] = 60;
@@ -69,10 +43,10 @@ class Board {
         piecesBoard[7][5] = 512;
 
         //Torres
-        piecesBoard[0][0] = 401;
-        piecesBoard[0][7] = 402;
-        piecesBoard[7][0] = 411;
-        piecesBoard[7][7] = 412;
+        piecesBoard[0][0] = 4010;
+        piecesBoard[0][7] = 4020;
+        piecesBoard[7][0] = 4110;
+        piecesBoard[7][7] = 4120;
 
         //Caballos
         piecesBoard[0][1] = 301;
@@ -87,10 +61,18 @@ class Board {
         for (i = 0; i < 8; i++) {
             for (j = 0; j < 8; j++) {
                 if(piecesBoard[i][j] != 2) {cout << " | " << piecesBoard[i][j];}
-                else cout << " | " << functionalBoard[i][j];
+                else {
+                    cout << " | ";
+                    if ((i + j) % 2 == 0) cout << "1";
+                    else cout << "0";
+                }
             }
             cout << " |" << endl << " +---+---+---+---+---+---+---+---+" << endl;
         }
+    }
+
+    void possibleMovs(int id){
+
     }
 
     void menacePositions(int color){
@@ -138,29 +120,93 @@ class Board {
     return 0; //No está en jaque
     }
 
+    //Verificar si el rey del color contrario están en mate
     int verificarMate(int color){
         int check = 0;
+        int kr = -1;
+        int cr = -1;
 
         for(i = 0; i < 8; i++){
             for(j = 0; j < 8; j++){
                 pieza = to_string(piecesBoard[i][j]);
                 if(pieza[0] == 7 && pieza[1] == !color){
+                    kr = i;
+                    cr = j;
                     break;
                 }
             }
+            if(k1 != -1) break;
         }
 
         menacePositions(color);
 
         if(color){
-            if(i > 0){
-                //Fijarse arriba
-                if(j > 0){
-                    //Fijarse izquierda
-                    //Fijarse arriba a la izquierda
+            if(menaceBlackBoard[kr][kc]) check++;
+            else if (kr > 0) {
+                if(menaceBlackBoard[kr-1][kc]) check++;
+                if (kc > 0) {
+                    if(menaceBlackBoard[kr-1][j-1]) check++;
+                }
+                if (kc < 7) {
+                    if(menaceBlackBoard[kr-1][kc+1]) check++;
+                    // Fijarse arriba a la derecha
+                }
+            } else if (kr < 7) {
+                if(menaceBlackBoard[kr+1][kc]) check++;
+                // Fijarse abajo
+                if (kc > 0) {
+                    if(menaceBlackBoard[kr+1][kc+1]) check++;
+                    // Fijarse abajo a la izquierda
+                }
+                if (kc < 7) {
+                    if(menaceBlackBoard[kr+1][kc+1]) check++;
+                    // Fijarse abajo a la derecha
+                }
+            } else {
+                if (kc > 0) {
+                    if(menaceBlackBoard[kr][kc-1]) check++;
+                    // Fijarse a la izquierda
+                }
+                if (kc < 7) {
+                    if(menaceBlackBoard[kr][kc+1]) check++;
+                    // Fijarse a la derecha
                 }
             }
+
         }
+        else if(menaceWhiteBoard[kr][j]) check++;
+            else if (i > 0) {
+                if(menaceWhiteBoard[i-1][j]) check++;
+                if (j > 0) {
+                    if(menaceWhiteBoard[i-1][j-1]) check++;
+                }
+                if (j < 7) {
+                    if(menaceWhiteBoard[i-1][j+1]) check++;
+                    // Fijarse arriba a la derecha
+                }
+            } else if (i < 7) {
+                if(menaceWhiteBoard[i+1][j]) check++;
+                // Fijarse abajo
+                if (j > 0) {
+                    if(menaceWhiteBoard[i+1][j+1]) check++;
+                    // Fijarse abajo a la izquierda
+                }
+                if (j < 7) {
+                    if(menaceWhiteBoard[i+1][j+1]) check++;
+                    // Fijarse abajo a la derecha
+                }
+            } else {
+                if (j > 0) {
+                    if(menaceWhiteBoard[i][j-1]) check++;
+                    // Fijarse a la izquierda
+                }
+                if (j < 7) {
+                    if(menaceWhiteBoard[i][j+1]) check++;
+                    // Fijarse a la derecha
+                }
+            }
+        if(check == 9) return 1;
+        else return 0;
     }
 
     void moverPieza(int tablero[8][8], int id) {
